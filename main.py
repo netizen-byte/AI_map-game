ed55caba243b8bcbccdd54e4098cbc6fdc252973
Cheng Strange <chengstrange@Chengs-MacBook-Pro.local>
2025-08-29 20:33:48 +0700

    UCS added

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
     pygame.display.set_caption("Dungeon escape")
 
-    game = Game(screen, room_json="room1.json")
+    game = Game(screen)
     running = True
     while running:
         running = game.run_step()
